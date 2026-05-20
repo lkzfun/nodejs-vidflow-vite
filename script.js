2328c02373043fd79a04d9995099eaf6cf5d0be9
lkzfun <lucasemanuelpires3@gmail.com>
2026-05-20 18:57:43 -0300

    Change logged URL to production URL directly
    Log the production URL directly instead of the variable.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ async function buscarEMostrarVideos() {
     ? "https://gist.githubusercontent.com/antonio-evaldo/e8a63621b51c883931eb3fa3a3eca990/raw/12f5c46ee6dd00d03c051adadaf341e06452cea0/videos.txt"
     : "http://localhost:3000/videos";
   console.log(import.meta.env.PROD);
-  console.log(urlVideos);
+  console.log("https://gist.githubusercontent.com/antonio-evaldo/e8a63621b51c883931eb3fa3a3eca990/raw/12f5c46ee6dd00d03c051adadaf341e06452cea0/videos.txt");
   try {
     const busca = await axios.get(urlVideos);
     const videos = busca.data;
